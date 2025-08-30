e81df73e26a2ca0d312988d5ecebf973cb6b80b7
Deron Martin <deronjudemartin@gmail.com>
2025-08-31 04:13:14 +0530

    Add game loop and improve input handling
    Introduce a game loop that allows the player to make multiple guesses until
    the correct number is found. Enhance input handling to gracefully manage
    non-numeric entries, prompting the user to re-enter their guess instead of
    crashing. The secret number is no longer revealed at the start.

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -8,23 +8,30 @@ fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    println!("The secret number is: {secret_number}");
 
-    println!("Please enter your guess: ");
+    loop {
+        println!("Please enter your guess: ");
 
-    let mut guess = String::new(); //::new() means that new() is an associated function
+        let mut guess = String::new(); //::new() means that new() is an associated function
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line!");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line!");
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    println!("You guessed: {guess}");
+        println!("You guessed: {guess}");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
-        Ordering::Less => println!("Too small!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Less => println!("Too small!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
