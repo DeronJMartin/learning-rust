2bee45b5f1cadea13fec14981e595348ff3402c0
Deron Martin <deronjudemartin@gmail.com>
2025-08-31 04:03:35 +0530

    Implement core guessing game logic
    Generate a random secret number for the player to guess.
    Parse the user's input into an integer.
    Compare the guess against the secret number and provide feedback
    (too high, too low, or correct).

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,8 +1,15 @@
 // Prelude - bring libraries into scope
+use std::cmp::Ordering;
 use std::io;
+
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("The secret number is: {secret_number}");
+
     println!("Please enter your guess: ");
 
     let mut guess = String::new(); //::new() means that new() is an associated function
@@ -11,5 +18,13 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line!");
 
-    println!("You guessed: {guess}")
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+    println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+        Ordering::Less => println!("Too small!"),
+    }
 }
